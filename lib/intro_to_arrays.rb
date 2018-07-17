ef8cdde1f940fe31aac9a8c4402b88290d573923
Justin Wang <justinw827@aol.com>
2018-07-17 21:28:36 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,15 @@
+def instantiate_new_array
+  my_new_array = Array.new
+end
+
+def array_with_two_elements
+  my_two_array = ["one", "two"]
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
